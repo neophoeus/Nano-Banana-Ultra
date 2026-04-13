1e2242cb87c771b5af4312a7b1d2736d86824c7f
neo <neo.genymt@gmail.com>
2026-04-13 09:26:09 +0800

    chore: untrack docs and add local helper scripts

---
+++ run_install.bat
@@ -0,0 +1,14 @@
+@echo off
+title NPM Installer
+echo.
+echo =======================================================
+echo Executing npm install, installing/updating project dependencies...
+echo This might take a few minutes. Please wait.
+echo =======================================================
+npm install
+echo.
+echo =======================================================
+echo Dependency installation complete!
+echo =======================================================
+echo.
+pause
